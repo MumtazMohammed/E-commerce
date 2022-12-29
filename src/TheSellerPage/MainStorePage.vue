e05c50fa1f669c27dff4562361bfca6a65159391
Mohammed <mumo7038@gmail.com>
2022-12-29 22:59:34 +0800

    first commit

--- src/TheSellerPage/MainStorePage.vue
+++ src/TheSellerPage/MainStorePage.vue
@@ -1,9 +1,57 @@
 <template>
   <div class="TheShowRoomPage">
     <v-container class="container">
+      <v-dialog v-model="dialog" fullscreen>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            elevation="0"
+            small
+            dark
+            class="btn blue darken-2 hidden-sm-and-up"
+          >
+            الأعلانات
+          </v-btn>
+        </template>
+        <v-sheet height="100%" class="white">
+          <v-card-title class="text py-1 grey--text text--darken-2">
+            <v-icon color="green lighten-2" right>mdi-bullhorn-outline</v-icon>
+            إعلان
+            <v-spacer></v-spacer>
+            <v-btn icon color="red">
+              <v-icon @click="dialog = false"> mdi-close-thick </v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-row no-gutters>
+            <v-col cols="12" class="pa-2" sm="6" md="4" lg="4">
+              <div>
+                <v-subheader class="Adv-titel pl-0 grey--text text--darken-1">
+                  هل أستطيع التعديل على التنسيقات؟
+                  <v-spacer></v-spacer>
+                    <v-icon light right size="15" color="red"> mdi-email</v-icon>
+                  <v-btn icon>
+                    <v-icon size="19" color=""> mdi-trash-can </v-icon>
+                  </v-btn>
+                </v-subheader>
+
+                <!-- <v-card-text class="Adv-text py-0 grey--text text--darken-1">
+                    بلا شك، يمكنك تغيير النصوص والصور، اختيار ألوانك الخاصة،
+                    ترتيب مكونات الصفحة بطريقتك، تغيير تقسيم الأعمدة… وأكثر، كل
+                    ذلك بالسحب والإدراج وملء الخانات. ويمكنك أيضاً استخدام
+                    التنسيق الواحد في أكثر من صفحة والتعديل عليها حسب ذوقك.
+                  </v-card-text> -->
+              </div>
+              <v-divider></v-divider>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-dialog>
+
+      <!--  -->
       <v-row no-gutters>
-        <v-col cols="12" class="">
-          <v-sheet rounded="lg" max-height="500" class="pa-3">
+        <v-col cols="12" class="hidden-md-and-down pa-2">
+          <v-card max-height="500" flat>
             <v-card-title class="text py-1 grey--text text--darken-2">
               <v-icon color="green lighten-2" right
                 >mdi-bullhorn-outline</v-icon
@@ -42,143 +90,142 @@
                 </v-card>
               </v-col>
             </v-row>
-          </v-sheet>
+          </v-card>
         </v-col>
-        <v-col class="py-2">
+        <v-col cols="12" class="pa-2">
           <!-- To Do list  -->
-          <v-row no-gutters>
-            <v-col
-              md="4"
-              lg="4"
-              sm="6"
-              cols="12"
-              v-for="(ToDo, i) in ToDoList"
-              :key="i"
-            >
-              <v-card class="ma-1 ToDoList-card">
-                <v-card-title class="subtitle">
-                  {{ ToDo.ToDoName }}
-                  <v-spacer></v-spacer>
-                  <span class="blue--text text--darken-1 subtitle">
-                    {{ ToDo.ToDoNumber }}
-                  </span>
-                </v-card-title>
-                <v-sheet class="mx-auto" max-width="300">
-                  <v-sparkline
-                    :value="value"
-                    :smooth="9"
-                    :padding="3"
-                    :line-width="6"
-                    stroke-linecap="round"
-                    :gradient="['#F4511E', '#FF7043']"
-                    type="trend"
-                    auto-line-width="true"
-                    auto-draw
-                    color="grey"
-                  >
-                  </v-sparkline>
-                </v-sheet>
-              </v-card>
-            </v-col>
-          </v-row>
-          <v-sheet flat class="mt-4 pa-2 overflow-hidden" rounded="lg">
+          <v-card flat class="overflow-hidden pa-2">
+            <v-row no-gutters>
+              <v-col
+                md="4"
+                lg="4"
+                sm="6"
+                cols="12"
+                v-for="(ToDo, i) in ToDoList"
+                :key="i"
+                class="pa-1"
+              >
+                <v-card class="ToDoList-card">
+                  <v-card-title class="subtitle pa-2">
+                    {{ ToDo.ToDoName }}
+                    <v-spacer></v-spacer>
+                    <span class="blue--text text--darken-1 subtitle">
+                      {{ ToDo.ToDoNumber }}
+                    </span>
+                  </v-card-title>
+                  <v-sheet class="mx-auto" max-width="300">
+                    <v-sparkline
+                      :value="value"
+                      :smooth="9"
+                      :padding="3"
+                      :line-width="6"
+                      stroke-linecap="round"
+                      :gradient="['#F4511E', '#FF7043']"
+                      type="trend"
+                      auto-line-width="true"
+                      auto-draw
+                      color="grey"
+                    >
+                    </v-sparkline>
+                  </v-sheet>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col class="pa-2" cols="12">
+          <v-card flat class="">
             <v-card-title
               class="Marketing-text justify-center white grey--text text--accent-3 py-2"
             >
               مركز التسويق والعروض
             </v-card-title>
-            <div class="py-3">
-              <v-row no-gutters>
-                <v-col class="pa1" cols="12" md="6" sm="m" lg="6">
-                  <v-card max-width="100%" class="pa-2">
-                    <v-card-title class="text deep-orange--text pa-2 py-0">
-                      عروض الجمعة
-                      <v-spacer></v-spacer>
-                      <v-btn
-                        small
-                        text
-                        class="enter-theOffer px-2"
-                        elevation="0"
-                        color="green accent-4"
-                      >
-                        دخول العرض
-                        <!-- <v-icon class="white--text">mdi-plus</v-icon> -->
-                      </v-btn>
-                    </v-card-title>
-                    <v-alert
-                      border="left"
-                      colored-border
-                      color="success"
+            <v-row no-gutters>
+              <v-col class="pa-3" cols="12">
+                <v-card max-width="100%" class="pa-2">
+                  <v-card-title class="text deep-orange--text pa-2 py-0">
+                    عروض الجمعة
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      small
+                      text
+                      class="enter-theOffer px-2"
                       elevation="0"
-                      class="ma-0 pa0"
-                      rounded="0"
+                      color="green accent-4"
                     >
-                      <small class="mb-1"> 22 / 05 / 2020 </small>
-                      <div class="text-offer">
-                        ابتداً من التاريخ الموضح سيتم نشر العروض في الساعة
-                        <span class="success--text mr-2">12:00 صباحاً</span>
-                      </div>
-                    </v-alert>
-                    <v-divider inset></v-divider>
-                    <v-alert
-                      border="left"
-                      colored-border
-                      color="error"
-                      elevation="0"
-                      class="ma-0"
-                      rounded="0"
-                    >
-                      <small class="mb-1"> 22 / 05 / 2020 </small>
-                      <div class="text-offer">
-                        ابتداً من التاريخ الموضح سيتم إغلاق العروض في الساعة
-                        <span class="error--text mr-1">12:00 مساً</span>
-                      </div>
-                    </v-alert>
-                  </v-card>
-                </v-col>
-              </v-row>
-            </div>
-            <v-card>
-              <v-card-title class="Marketing-titel pa-3">
-                التسويق
-              </v-card-title>
-              <v-sheet color="grey lighten-3" class="px-2 mb-5">
-                <v-row class="mt-0" fill-height>
-                  <v-col cols="12" sm="6" md="6" lg="6">
-                    <v-card flat height="200">
-                      <v-card-actions class="flex-column pb-0">
-                        <v-icon class="icon-Marketing">mdi-star</v-icon>
-                        <v-card-text class="text-center title-featur pa-2">
-                          المنتجات المتميزة
-                        </v-card-text>
-                      </v-card-actions>
-                      <v-card-text class="text-center text-featur pt-0 pa-2">
-                        المنتجات المتميزةالمنتجات المتميزةالمنتجات
-                        المتميزةالمنتجات المتميزةالمنتجات المتميزةالمنتجات
-                        المتميزةالمنتجات المتميزةالمنتجات المتميزة
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="6" lg="6">
-                    <v-card flat height="200">
-                      <v-card-actions class="flex-column pb-0">
-                        <v-icon class="icon-Marketing">
-                          mdi-ticket-percent
-                        </v-icon>
-                        <v-card-text class="text-center title-featur pa-2">
-                          قسائم
-                        </v-card-text>
-                      </v-card-actions>
-                      <v-card-text class="text-center text-featur pt-0 pa-2">
-                        قم بزيادة الطلبات من خلال تقديم أسعار مخفضة للمشترين عند
-                        الخروج باستخدام القسائم.
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                </v-row>
-              </v-sheet>
-            </v-card>
-          </v-sheet>
+                      دخول العرض
+                      <!-- <v-icon class="white--text">mdi-plus</v-icon> -->
+                    </v-btn>
+                  </v-card-title>
+                  <v-alert
+                    border="left"
+                    colored-border
+                    color="success"
+                    elevation="0"
+                    class="ma-0 pa0"
+                    rounded="0"
+                  >
+                    <small class="mb-1"> 22 / 05 / 2020 </small>
+                    <div class="text-offer">
+                      ابتداً من التاريخ الموضح سيتم نشر العروض في الساعة
+                      <span class="success--text mr-2">12:00 صباحاً</span>
+                    </div>
+                  </v-alert>
+                  <v-divider inset></v-divider>
+                  <v-alert
+                    border="left"
+                    colored-border
+                    color="error"
+                    elevation="0"
+                    class="ma-0"
+                    rounded="0"
+                  >
+                    <small class="mb-1"> 22 / 05 / 2020 </small>
+                    <div class="text-offer">
+                      ابتداً من التاريخ الموضح سيتم إغلاق العروض في الساعة
+                      <span class="error--text mr-1">12:00 مساً</span>
+                    </div>
+                  </v-alert>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col class="pa-2" cols="12">
+          <v-card flat>
+            <v-card-title class="Marketing-titel pa-3"> التسويق </v-card-title>
+            <v-row no-gutters>
+              <v-col class="pa-2" cols="12" sm="6" md="6" lg="6">
+                <v-card height="200">
+                  <v-card-actions class="flex-column pb-0">
+                    <v-icon class="icon-Marketing">mdi-star</v-icon>
+                    <v-card-text class="text-center title-featur pa-2">
+                      المنتجات المتميزة
+                    </v-card-text>
+                  </v-card-actions>
+                  <v-card-text class="text-center text-featur pt-0 pa-2">
+                    المنتجات المتميزةالمنتجات المتميزةالمنتجات المتميزةالمنتجات
+                    المتميزةالمنتجات المتميزةالمنتجات المتميزةالمنتجات
+                    المتميزةالمنتجات المتميزة
+                  </v-card-text>
+                </v-card>
+              </v-col>
+              <v-col class="pa-2" cols="12" sm="6" md="6" lg="6">
+                <v-card height="200">
+                  <v-card-actions class="flex-column pb-0">
+                    <v-icon class="icon-Marketing"> mdi-ticket-percent </v-icon>
+                    <v-card-text class="text-center title-featur pa-2">
+                      قسائم
+                    </v-card-text>
+                  </v-card-actions>
+                  <v-card-text class="text-center text-featur pt-0 pa-2">
+                    قم بزيادة الطلبات من خلال تقديم أسعار مخفضة للمشترين عند
+                    الخروج باستخدام القسائم.
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -195,6 +242,7 @@ export default {
       ToDolist: false,
       BusinessInsights: false,
       search: "",
+      dialog: false,
       ShowRoomUserCars,
       carId: this.$route.params.carId,
 
@@ -235,32 +283,7 @@ export default {
   @media (max-width: 600px) {
     margin-bottom: 50px;
   }
-  .text {
-    font-family: $fontfamliy3;
-    letter-spacing: 0 !important;
-    font-weight: 500;
-    font-size: 17px;
-    @media (max-width: 600px) {
-      font-size: 15px;
-    }
-    .v-icon {
-      font-size: 18px !important;
-    }
-  }
-  .Adv-titel {
-    font-family: $fontfamliy3;
-    letter-spacing: 0 !important;
-    font-weight: 600;
-    font-size: 14px;
-  }
-  .Adv-text {
-    font-family: $fontfamliy3;
-    letter-spacing: 0 !important;
-    font-weight: 400;
-    font-size: 13px;
-    line-height: 1.8;
-    text-align: justify;
-  }
+
   .text-offer {
     letter-spacing: 0 !important;
     font-family: $fontfamliy3;
@@ -314,6 +337,35 @@ export default {
     }
   }
 }
+// ::v-deep .v-dialog.v-dialog--active {
+//   margin: 5px;
+// }
+.Adv-titel {
+  font-family: $fontfamliy3;
+  letter-spacing: 0 !important;
+  font-weight: 500;
+  font-size: 14px;
+}
+.Adv-text {
+  font-family: $fontfamliy3;
+  letter-spacing: 0 !important;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 1.8;
+  text-align: justify;
+}
+.text {
+  font-family: $fontfamliy3;
+  letter-spacing: 0 !important;
+  font-weight: 500;
+  font-size: 17px;
+  @media (max-width: 600px) {
+    font-size: 15px;
+  }
+  .v-icon {
+    font-size: 18px !important;
+  }
+}
 .ToDoList-box {
   @media (max-width: 600px) {
     flex-direction: column;
@@ -346,5 +398,17 @@ export default {
 
 .offers-card::-webkit-scrollbar-thumb {
   background-color: darkgrey;
+}
+.btn {
+  position: fixed;
+  right: 3px;
+  font-family: $fontfamliy3 !important;
+  letter-spacing: 0 !important;
+  font-weight: 600;
+  z-index: 5;
+  opacity: 0.7;
+}
+.container {
+  padding: 0px !important;
 }
 </style>
